275541b5970a28569c507c648c95f7b74054fe91
Steven D. Patz <steven.patz@gmail.com>
2013-02-12 20:24:06 -0500

    Testing to see that paths are set correctly, and that SimplePie is working

--- pod.php
+++ pod.php
@@ -1,3 +1,22 @@
 <?php
 require("../SimplePie/simplepie_1.3.1.mini.php");
+//require_once('../SimplePie/autoloader.php');
+// @TODO Just a single feed for now, but soon it will be
+// a full html page with a form to select the feed to 
+// parse
+
+$testFeed = "http://www.loc.gov/podcasts/exquisitecorpse/exquisitecorpse.xml";
+$feed = new SimplePie();
+ 
+// Set which feed to process.
+$feed->set_feed_url($testFeed);
+ 
+// Run SimplePie.
+$feed->init();
+ 
+// This makes sure that the content is sent to the browser as text/html and the UTF-8 character set (since we didn't change it).
+$feed->handle_content_type();
+print $feed->get_description();
+
+
 ?>
