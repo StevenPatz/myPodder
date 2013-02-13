138b9d3820c365a59dc82620d5ec6b36b8fe4d6a
Steven D. Patz <steven.patz@gmail.com>
2013-02-12 20:41:05 -0500

    Go through the feed and print out the links to the actual mp3. Also added code to determine if running from the command line, and if so use the proper character for a new line, else use a br as we're on a webpage.

--- pod.php
+++ pod.php
@@ -4,6 +4,11 @@ require("../SimplePie/simplepie_1.3.1.mini.php");
 // @TODO Just a single feed for now, but soon it will be
 // a full html page with a form to select the feed to 
 // parse
+if (PHP_SAPI === 'cli') {
+	$nlbr = "\n";
+   } else {
+	$nlbr = "<br/>";
+}
 
 $testFeed = "http://www.loc.gov/podcasts/exquisitecorpse/exquisitecorpse.xml";
 $feed = new SimplePie();
@@ -17,6 +22,13 @@ $feed->init();
 // This makes sure that the content is sent to the browser as text/html and the UTF-8 character set (since we didn't change it).
 $feed->handle_content_type();
 print $feed->get_description();
-
+print $nlbr;
+foreach ($feed->get_items() as $item) {
+	$enclosures = $item->get_enclosures();
+	foreach ($enclosures as $enclosure) {
+		print $enclosure->get_link();
+		print $nlbr;
+	}
+}
 
 ?>
